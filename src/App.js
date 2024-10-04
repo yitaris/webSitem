6ffdfbfcf6e918d674b0f703f3a505de0822a991
Yiğit Bayır <yitaris>
2024-10-04 13:07:39 +0300

    rel-8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
